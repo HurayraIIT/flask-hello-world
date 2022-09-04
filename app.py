b4bc04f4879a94163de63b8e03bf678d0583c662
Abu Hurayra <hurayraiit@gmail.com>
2022-09-04 16:06:50 +0600

    added static files

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, request, render_template, url_for, send_from_directory
 import requests
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
+application = app
 
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/robots.txt')
+def static_from_root():
+    return send_from_directory(app.static_folder, request.path[1:])
 
 
 if __name__ == "__main__":
